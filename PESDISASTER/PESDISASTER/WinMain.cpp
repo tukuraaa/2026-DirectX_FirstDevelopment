722f039b1fdd14b26bef2a45a62bb10ecc3d5335
nakayama_yuto <udoyama5@gmail.com>
2026-05-12 21:53:20 +0900

    [configure]I configured the system to allow the installation of the DX library.
    変更内容：DXライブラリを入れられるように設定した。

--- PESDISASTER/PESDISASTER/WinMain.cpp
+++ PESDISASTER/PESDISASTER/WinMain.cpp
@@ -1,80 +1,89 @@
-#include <windows.h>
-#include "LogicManager.h"
+#include "DxLib.h"
+#include <cstdlib>
+#include <ctime>
 
-/// <summary>
-/// ウィンドウのメッセージを処理する関数
-/// </summary>
-/// <param name="hwnd"></param>
-/// <param name="uint"></param>
-/// <param name="wparam"></param>
-/// <param name="lparam"></param>
-/// <returns></returns>
-LRESULT CALLBACK WindowProc(HWND _hwnd, UINT _uint, WPARAM _wparam, LPARAM _lparam)
-{
-	// もしウィンドウが破棄された場合
-	if (_uint == WM_DESTROY)
-	{
-		PostQuitMessage(0);// アプリケーションを終了するためのメッセージを送る
-		return 0;
-	}
+// ゲームの状態
+enum class GameState { Wait, Play, Clear };
 
-	return DefWindowProc(_hwnd, _uint, _wparam, _lparam);// デフォルトのウィンドウプロシージャを呼び出す
-}
+// プログラムの入り口（WinMain）
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
-/// <summary>
-/// ゲームを開始するためのエントリーポイント関数
-/// </summary>
-/// <param name="hInstance"></param>
-/// <param name="hPrevInstance"></param>
-/// <param name="lpCmdLine"></param>
-/// <param name="nCmdShow"></param>
-/// <returns></returns>
-int WINAPI WinMain(HINSTANCE _hinstance_Instance, HINSTANCE _hinstancePrev_Instance, LPSTR _lpstr, int _nCmdShow)
-{
-	// ウィンドウの登録と作成
-	const wchar_t _cLASS_NAME[] = L"MyGameWindow";// ウィンドウクラスの構造体を初期化
-	WNDCLASS _wndclass = {};// ウィンドウプロシージャを設定
-	_wndclass.lpfnWndProc = WindowProc;// ウィンドウクラスのインスタンスを設定
-	_wndclass.hInstance = _hinstance_Instance;// ウィンドウクラスの名前を設定
-	_wndclass.lpszClassName = _cLASS_NAME;// ウィンドウクラスを登録
-	RegisterClass(&_wndclass);// ウィンドウを作成
-	HWND _hwnd = CreateWindowEx(0, _cLASS_NAME, L"DirectX12 R-Key Game", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, nullptr, nullptr, _hinstance_Instance, nullptr);// ウィンドウのハンドルを取得
+    // ウィンドウモードで起動する（これがないと全画面になります）
+    ChangeWindowMode(TRUE);
 
-	// もしウィンドウの作成に失敗した場合
-	if (_hwnd == NULL)
-	{
-		return 0;
-	}
+    // DXライブラリの初期化（ウィンドウの作成やDirectX11の準備を全部やってくれます）
+    if (DxLib_Init() == -1) { return -1; }
 
-	ShowWindow(_hwnd, _nCmdShow);// ウィンドウを表示
+    // 描画先を裏画面にする（画面のチラつき防止）
+    SetDrawScreen(DX_SCREEN_BACK);
 
-	// ゲームの準備
-	LogicManager _logic;// ゲームロジックのインスタンスを作成
-	MSG _msg = {};// メッセージ構造体を初期化
+    // --- ゲームの準備 ---
+    GameState state = GameState::Wait;
+    int targetKey = 0;       // DxLibのキーコード
+    char targetKeyChar = ' '; // 画面表示用の文字
 
-	// ゲームループ
-	while (_msg.message != WM_QUIT)
-	{
-		// もしメッセージがある場合
-		if (PeekMessage(&_msg, nullptr, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&_msg);// メッセージを翻訳
-			DispatchMessage(&_msg);// メッセージをウィンドウプロシージャに送る
-		}
-		else
-		{
-			_logic.Update();// ロジックの更新
+    // 候補のキー（DXライブラリ専用のキーコードを使います）
+    int candidateKeys[] = { KEY_INPUT_A, KEY_INPUT_B, KEY_INPUT_C, KEY_INPUT_X, KEY_INPUT_Y };
+    char candidateChars[] = { 'A', 'B', 'C', 'X', 'Y' };
+    int candidateCount = 5;
 
-			// ② 描画処理（DirectX 12のコマンドを積んで画面に映す）
-			// Render(logic); 
+    srand((unsigned)time(NULL));
 
-			// もし表示するテキストがある場合
-			if (_logic.GetTargetKeyText() != L"")
-			{
-				SetWindowText(_hwnd, (L"ターゲットキー: " + _logic.GetTargetKeyText()).c_str());// ウィンドウのタイトルを更新
-			}
-		}
-	}
+    // --- メインループ ---
+    // ProcessMessage: Windowsの裏側の処理、CheckHitKey: ESCで終了
+    while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
 
-	return 0;
+        ClearDrawScreen(); // 画面を真っ黒にクリア
+
+        // 【1. ロジック（状態の更新）】
+        switch (state) {
+        case GameState::Wait:
+            // Rキーが押されたか
+            if (CheckHitKey(KEY_INPUT_R)) {
+                int r = rand() % candidateCount;
+                targetKey = candidateKeys[r];
+                targetKeyChar = candidateChars[r];
+                state = GameState::Play;
+            }
+            break;
+
+        case GameState::Play:
+            // 指定されたキーが押されたか
+            if (CheckHitKey(targetKey)) {
+                state = GameState::Clear;
+            }
+            break;
+
+        case GameState::Clear:
+            // スペースキーでリトライ
+            if (CheckHitKey(KEY_INPUT_SPACE)) {
+                state = GameState::Wait;
+            }
+            break;
+        }
+
+        // 【2. 描画処理】
+        // 常に表示（文字を出すのが信じられないくらい簡単です！）
+        DrawString(10, 10, "Rボタンを押してください (ESCで終了)", GetColor(255, 255, 255));
+
+        switch (state) {
+        case GameState::Wait:
+            break;
+
+        case GameState::Play:
+            // 変数を使って文字を描画する
+            DrawFormatString(300, 240, GetColor(255, 255, 0), "指定キー: %c を押せ！", targetKeyChar);
+            break;
+
+        case GameState::Clear:
+            DrawString(250, 240, "ゲームクリア！ (Spaceでリトライ)", GetColor(0, 255, 255));
+            break;
+        }
+
+        ScreenFlip(); // 裏画面を表画面に反映
+    }
+
+    // DXライブラリの終了処理
+    DxLib_End();
+    return 0;
 }
